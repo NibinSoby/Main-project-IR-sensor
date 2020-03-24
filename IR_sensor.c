7a90e48ba3962c454f2d00ac04507a0df9c6f24a
NibinSoby <55203643+NibinSoby@users.noreply.github.com>
2020-03-24 19:23:37 -0400

    Update IR_sensor.c

--- IR_sensor.c
+++ IR_sensor.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
-#include <wiringPi.h>
-#include <wiringPiI2C.h>
-#include <fstream>
-#include <iostream>
-#include <thread>
-#include <chrono>
+#include<stdio.h>
+#include <wiringPi.h>     // to access gpio pins
+#include <wiringPiI2C.h>   // to control I2c lines
+#include <fstream>        // to store updated file
+#include <iostream>      // basic input output
+#include <thread> 
+#include <chrono>       
 
-
-const int led = 16;
-const int proximity = 23;
+const int led = 16;       // pin number of led
+const int proximity = 23; // pin number for proximity
 const int buzzer = 12;
 uint8_t LastStatus = 1;
-uint8_t EmergencyStop= 21;
+uint8_t EmergencyStop= 21;// to stop in case of error
 
 
 int main(void)
 {
   
   
-  wiringPiSetupGpio();
-  pinMode(led, OUTPUT);
-  pinMode(buzzer, OUTPUT);  
-  pullUpDnControl(proximity,PUD_DOWN);
+  wiringPiSetupGpio();    // function to set gpio 
+  pinMode(led, OUTPUT);   // led pin as output
+  pinMode(buzzer, OUTPUT);  // set as output
+  pullUpDnControl(proximity,PUD_DOWN); // as sensor gives high signal when it sense any surface ,when sensor is in off state some times it disconnected . so to avoid the floating state of pin and to stabilized functionality of the input pin for proximity sensor. We set to pull down internally using this pullupdncontrol() function. This sets input pin to ground via 10k resistor if we write PUD_DOWN in the argument. So pin always get ground signal until high signal comes from the sensor .
   pullUpDnControl(EmergencyStop,PUD_DOWN);
    
        for(int i=0; i<2; i++)
     {
-      digitalWrite(buzzer, HIGH);
+      digitalWrite(buzzer, HIGH); // sets output pin to +5v
       digitalWrite(led, HIGH);
-      delay(100);
+      delay(100);                //Set a time delay before going off
       digitalWrite(led, LOW);
       digitalWrite(buzzer, LOW);
       delay(100);
@@ -37,7 +36,9 @@ int main(void)
     while(1)
   {
     
-  if(digitalRead(proximity) && LastStatus==0)
+  if(digitalRead(proximity) && LastStatus==0) // && this " AND " operator and reads the and value from input
+
+ 
   {
     
     digitalWrite(led, HIGH);
@@ -46,7 +47,7 @@ int main(void)
   }
     
   
-      else if(!digitalRead(proximity) && LastStatus==1)
+      else if(!digitalRead(proximity) && LastStatus==1) // digitalRead() = reads the value from input pin
   {
        
        digitalWrite(led, LOW);
@@ -62,5 +63,6 @@ int main(void)
       
    }// end of while loop
 }
+
  
 
